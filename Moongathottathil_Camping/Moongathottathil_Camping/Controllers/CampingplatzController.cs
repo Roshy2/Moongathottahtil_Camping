6c3ac5ad63b066e5e16706939712ad7cf86ef1d0
Roshy2 <roshy@live.at>
2020-03-31 17:26:11 +0200

    Projekt so gut wie Fertig

---
+++ Moongathottathil_Camping/Moongathottathil_Camping/Controllers/CampingplatzController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Moongathottathil_Camping.Controllers
+{
+    public class CampingplatzController : Controller
+    {
+        // GET: Campingplatz
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Infos()
+        {
+            return View();
+        }
+        public ActionResult Preise()
+        {
+            return View();
+        }
+        public ActionResult Bilder()
+        {
+            return View();
+        }
+    }
+}
---
+++ Moongathottathil_Camping/Moongathottathil_Camping/Controllers/ReservierungController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Moongathottathil_Camping.Controllers
+{
+    public class ReservierungController : Controller
+    {
+        // GET: Reservierung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Anfrage()
+        {
+            return View();
+        }
+    }
+}
